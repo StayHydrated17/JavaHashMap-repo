50cb1b9bcb59de9e6dab4693bf3d768f0d0053dd
Alberto PogChamp <drinksomewater17@gmail.com>
2021-03-15 14:22:09 -0700

    Update

--- HashMaps.java
+++ HashMaps.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+//importing the hashmap library
+
 //@author github.com/StayHydrated17
 //@date 3/15/2021
 
@@ -5,7 +8,11 @@ public class HashMaps
 {    
     public static void main(String[] args)
     {
-        
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        HashMap myHash = new HashMap();
 
     }
     
